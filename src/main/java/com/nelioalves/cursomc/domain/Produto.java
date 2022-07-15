be720d784cc862c59db6d3b81b800bf37d7a556c
Alex Arraes <alex-junior1@hotmail.com>
2022-07-15 15:39:53 -0300

    AJUSTES NO ENDPOINT /CATEGORIAS{ID}

--- src/main/java/com/nelioalves/cursomc/domain/Produto.java
+++ src/main/java/com/nelioalves/cursomc/domain/Produto.java
@@ -13,6 +13,9 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 
 @Entity
 public class Produto implements Serializable{
@@ -24,6 +27,7 @@ public class Produto implements Serializable{
 	private String nome;
 	private Double preco;
 	
+	@JsonBackReference
 	@ManyToMany
 	@JoinTable(name= "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name="produto_id"), inverseJoinColumns = @JoinColumn(name="categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
@@ -63,7 +67,8 @@ public class Produto implements Serializable{
 	public void setPreco(Double preco) {
 		this.preco = preco;
 	}
-
+	
+	@JsonIgnore
 	public List<Categoria> getCategorias() {
 		return categorias;
 	}
